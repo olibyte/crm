5bf0e8c7767062296638745cfba1141a4d836a0a
Oliver <ocben1@gmail.com>
2019-06-25 17:33:03 +1000

    add server controller for CORS

---
+++ src/main/java/com/crm/crm/model/ContactsController.java
@@ -0,0 +1,12 @@
+package com.crm.crm.model;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
+class ContactsController {
+    
+}
